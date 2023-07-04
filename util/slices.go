32598469db7d07f1459f7651006777546cde46fd
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-07-04 19:26:12 +0800

    feat: 添加元组过滤方法

--- util/slices.go
+++ util/slices.go
@@ -7,3 +7,12 @@ func Reduce[T, M any](s []T, f func(M, T) M, initValue M) M {
 	}
 	return acc
 }
+
+func Filter[T any](d, s []T, keep func(i T) bool) []T {
+	for _, n := range s {
+		if keep(n) {
+			d = append(d, n)
+		}
+	}
+	return d
+}
