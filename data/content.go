dc13eff5625ca770555b351ab5254d9967fb9cbd
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-07-06 11:41:35 +0800

    feat: 调整数据结构

--- data/content.go
+++ data/content.go
@@ -4,11 +4,10 @@ import "github.com/ichaly/gcms/base"
 
 type Content struct {
 	Title       string `gorm:"size:200;comment:标题"`
-	Description string `gorm:"size:200;comment:简介"`
 	Content     string `gorm:"type:text;comment:内容"`
+	Description string `gorm:"size:1000;comment:简介"`
 	Source      string `gorm:"comment:来源"`
 	Kind        Kind   `gorm:"comment:类型"`
-	Media       []Media
 	base.DeleteEntity
 }
 
