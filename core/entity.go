416047e46fb22b1eae55004790d69b14d28193a7
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-07-31 22:36:59 +0800

    feat: support auto auditor

--- core/entity.go
+++ core/entity.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+type userContextKeyType struct{}
+
+var UserContextKey = userContextKeyType{}
+
 type ID uint64
 
 func (my ID) ID() {}
@@ -31,8 +35,37 @@ type AuditorEntity struct {
 	UpdatedBy *uint64 `gorm:"comment:更新人;"`
 }
 
+func (my *AuditorEntity) BeforeCreate(tx *gorm.DB) error {
+	if val, ok := getCurrentUserFromContext(tx); ok {
+		my.CreatedBy = val
+	}
+	return nil
+}
+
+func (my *AuditorEntity) BeforeUpdate(tx *gorm.DB) error {
+	if val, ok := getCurrentUserFromContext(tx); ok {
+		my.UpdatedBy = val
+	}
+	return nil
+}
+
 type DeleteEntity struct {
 	AuditorEntity `mapstructure:",squash"`
 	DeletedBy     *uint64        `gorm:"comment:删除人;"`
 	DeletedAt     gorm.DeletedAt `gorm:"index;comment:逻辑删除;"`
+}
+
+func (my *DeleteEntity) BeforeDelete(tx *gorm.DB) error {
+	if val, ok := getCurrentUserFromContext(tx); ok {
+		my.UpdatedBy = val
+	}
+	return nil
+}
+
+func getCurrentUserFromContext(tx *gorm.DB) (*uint64, bool) {
+	ctx := tx.Statement.Context
+	if val, ok := ctx.Value(UserContextKey).(uint64); ok && val > 0 {
+		return &val, ok
+	}
+	return nil, false
 }
